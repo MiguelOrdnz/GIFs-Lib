28f82bfa6f45e53ac8ae7f19fa607e6dbb36039b
Miguel Ordóñez <miguel.ord94@gmail.com>
2020-12-06 15:03:10 -0600

    Display gifs items component

--- src/GiphyPlayground.js
+++ src/GiphyPlayground.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GiphyPlayground = () => {
-  const [categories, setCategories] = useState(["Samurai X", "Hinuyasha", "Digimon"])
-
-  // const handleAddCategory = () => setCategories([...categories, 'anotherone'])
+  const [categories, setCategories] = useState(["Samurai X"])
   
   return (
     <div>
       <h1>Giphy Playground</h1>
       <AddCategory setCategories = { setCategories }/>
       <hr />
-      <ol>
-        {categories.map(( category ) => (
-          <li key={ category }> { category } </li>
-        ))}
-      </ol>
+      <div>
+        {
+          categories.map(( category ) => (
+              <GifGrid 
+                key={ category }
+                category={ category }/>
+            )
+          )
+        }
+      </div>
     </div>
   );
 };
