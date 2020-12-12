ae227f6b54f16afb41f82cc7042b8c92f98e4526
Miguel Ordóñez <miguel.ord94@gmail.com>
2020-12-11 21:12:02 -0600

    Playing with memoization hooks

--- src/GiphyPlayground.js
+++ src/GiphyPlayground.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { HooksPlayground } from "./hooksPlayground/HooksPlayground";
-import { GiphyPg } from './components/GiphyPg'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from 'react-router-dom'
+import { HooksPlayground } from "./hooksPlayground/HooksPlayground";
+import { GiphyPg } from './components/GiphyPg'
 import { BreakingBadAPI } from "./breaking_bad_api/BreakingBadAPI";
-
+import { Padre } from './hooksPlayground/07-tarea-memo/Padre'
 
 
 export const GiphyPlayground = () => {  
@@ -25,6 +25,9 @@ export const GiphyPlayground = () => {
           <li>
             <Link to="/BreakingBad">Breakign Bad API</Link>
           </li>
+          <li>
+            <Link to="/tareaMemo">Tarea Memo</Link>
+          </li>
         </ul>
       </div>
       <Switch>
@@ -36,6 +39,9 @@ export const GiphyPlayground = () => {
         </Route>
         <Route path='/BreakingBad'>
           <BreakingBadAPI />
+        </Route>
+        <Route path='/tareaMemo'>
+          <Padre />
         </Route>
       </Switch>
     </Router>
