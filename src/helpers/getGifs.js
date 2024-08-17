5ed9e320465ce1c209bd286340e09bcc0ea9b352
mgdnz <miguel.ord94@gmail.com>
2024-08-16 20:39:14 -0600

    Manage secrets

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
         
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=hUq37hgpUBW71UFF19Q85BmzkT062RiA&limit=10&q=${encodeURI(category)}`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&limit=10&q=${encodeURI(category)}`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
